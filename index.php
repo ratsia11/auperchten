e289c3c80b32c4719d51fc3d9b845bf600124040
ratsia11 <srathkolb@gmail.com>
2017-10-12 21:49:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<?php
+include('subsites/style.php');
+ ?>
+<head>
+  <title>Au-Perchten</title>
+  <script src="imports/jquery-3.2.1.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"/>
+  <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" ></script>
+  <style>
+body {background-color: black; color: white;}
+  </style>
+</head>
+<body>
+<div class="logo">
+  <li><a href="index.php"><img src="images/logo.png"/></a></li>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="subsites/events.php">Veranstaltungen</a></li>
+  <li><a href="subsites/members.php">Termine</a></li>
+  <li><a href="subsites/news.php">Aktuelles</a></li>
+  <li><a href="subsites/members.php">Kontakt</a></li>
+
+
+</div>
+</body>
+</html>
---
+++ subsites/style.php
@@ -0,0 +1,26 @@
+<style>
+
+.logo{
+  height: 200px;
+  width: 100%;
+  float: left;
+  padding: 5px 5px 5px 5px;
+  display: inline-block;
+}
+
+.logo li{
+  height: 200px;
+  margin-top: 0;
+  display: inline-block;
+  padding-right: 20px;
+  font-size: 25px;
+  text-decoration: -moz-none;
+}
+.logo li a{
+    color: #FFFFFF;
+    text-decoration: none;
+}
+
+
+
+</style>
